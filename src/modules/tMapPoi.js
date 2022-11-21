a78c8d1bb44bcd9c5a218e8170e4e7c3538c23eb
kshull <rhkstn0303@naver.com>
2022-11-21 10:26:36 +0900

    storage 추가

--- src/modules/tMapPoi.js
+++ src/modules/tMapPoi.js
@@ -7,22 +7,24 @@ const AppKey = 'l7xxaa988ec5c97f499186a2545e0ab433fe';
 // https://apis.openapi.sk.com/tmap/pois?version={version}&page={page}&count={count}&searchKeyword={searchKeyword}&areaLLCode={areaLLCode}&areaLMCode={areaLMCode}&resCoordType={resCoordType}&searchType={searchType}&searchtypCd={searchtypCd}&radius={radius}&reqCoordType={reqCoordType}&centerLon={centerLon}&centerLat={centerLat}&multiPoint={multiPoint}&callback={callback}&appKey={appKey}
 
 export async function requestPoi(searchText) {
-  if (searchText == null || searchText == '') return;
+    if (searchText == null || searchText == '') return;
 
-  let url = BaseUrl + `pois?version=1&count=200&radius=0&appkey=${AppKey}&searchKeyword=${searchText}`;
+    let url =
+        BaseUrl +
+        `pois?version=1&count=200&radius=0&appkey=${AppKey}&searchKeyword=${searchText}`;
 
-  const response = await axios.get(url).catch(error => {
-    console.error(error);
-    throw error;
-  });
+    const response = await axios.get(url).catch(error => {
+        console.error(error);
+        throw error;
+    });
 
-  if (response == undefined) return null;
+    if (response == undefined) return null;
 
-  console.log(
-    'response totalCount >> ' + response.data.searchPoiInfo.totalCount,
-  );
-  console.log('response count >> ' + response.data.searchPoiInfo.count);
-  console.log('response page >> ' + response.data.searchPoiInfo.page);
+    console.log(
+        'response totalCount >> ' + response.data.searchPoiInfo.totalCount,
+    );
+    console.log('response count >> ' + response.data.searchPoiInfo.count);
+    console.log('response page >> ' + response.data.searchPoiInfo.page);
 
-  return response.data.searchPoiInfo.pois.poi;
+    return response.data.searchPoiInfo.pois.poi;
 }
--- src/view/poiView.js
+++ src/view/poiView.js
@@ -1,122 +1,143 @@
 import React from 'react';
 import {
-  Button,
-  FlatList,
-  TextInput,
-  View,
-  ActivityIndicator,
-  Text,
-  StyleSheet,
+    Button,
+    FlatList,
+    TextInput,
+    View,
+    ActivityIndicator,
+    Text,
+    StyleSheet,
+    TouchableWithoutFeedback
 } from 'react-native';
 import {requestPoi} from '../modules/tMapPoi';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import { storeDestination } from "../modules/tmapStorage"
+
+
+/* 
+    뒤로가기 방법
+    import { StackActions } from '@react-navigation/native';
+
+    const popAction = StackActions.pop(1);
+
+    navigation.dispatch(popAction);
+ */
+
 
 const PoiView = () => {
-  const [loading, setLoading] = useState(false);
-  const [res, setRes] = useState(null);
-  const [searchText, setText] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [res, setRes] = useState(null);
+    const [searchText, setText] = useState('');
 
-  /* useEffect(() => {
-        console.log('useEffect');
-        if (res != undefined || res != null) {
-            res.map(data => {
-                console.log('name >>> ' + data.name);
-            });
+    async function request() {
+        setLoading(true);
+
+        const response = await requestPoi(searchText).catch(reason => {
+            console.log(reason);
+        });
+
+        if (response != undefined) {
+            console.log('response is not undefined ');
+            setRes(response);
         }
-    }, [res]); */
 
-  async function request() {
-    console.log('text >> ' + searchText);
-    setLoading(true);
-
-    const response = await requestPoi(searchText).catch(reason => {
-      console.log(reason);
-    });
-
-    if (response != undefined) {
-      console.log('response is not undefined ');
-      setRes(response);
+        setLoading(false);
     }
 
-    setLoading(false);
-  }
+    function onTextChange(str) {
+        setText(str);
+        request();
+    }
+    
+    function onItemSel(data) {
+        const saveData = {
+            addr: data.newAddress[0].fullAddressRoad,
+            lat: data.noorLat,
+            lon: data.noorLon,
+            name: data.name
+        }
+        storeDestination(saveData)
+    }
 
-  function onTextChange(str) {
-    setText(str);
-    request();
-  }
+    function cellRender(item) {
+        return (
+            <TouchableWithoutFeedback style={styles.cellDivider} onPress={()=>onItemSel(item.item)}>
+                <View style={styles.cell}>
+                    <Text style={styles.cellText}>이름: {item.item.name}</Text>
+                    {item.item.newAddressList ? (
+                        <Text style={styles.cellText}>
+                            주소:{' '}
+                            {
+                                item.item.newAddressList.newAddress[0]
+                                    .fullAddressRoad
+                            }
+                        </Text>
+                    ) : (
+                        <Text style={styles.cellText}>
+                            주소: {item.item.upperAddrName}{' '}
+                            {item.item.middleAddrName} {item.item.lowerAddrName}{' '}
+                            {item.item.detailAddrName}
+                        </Text>
+                    )}
+                    <Text>
+                        좌표 Lat {item.item.noorLat} / Lon {item.item.noorLon}
+                    </Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
 
-  function cellRender(item) {
     return (
-      <View style={styles.cellDivider}>
-        <View style={styles.cell}>
-          <Text style={styles.cellText}>이름: {item.item.name}</Text>
-          {item.item.newAddressList ? (
-            <Text style={styles.cellText}>
-              주소: {item.item.newAddressList.newAddress[0].fullAddressRoad}
-            </Text>
-          ) : (
-            <Text style={styles.cellText}>
-              주소: {item.item.upperAddrName} {item.item.middleAddrName}{' '}
-              {item.item.lowerAddrName} {item.item.detailAddrName}
-            </Text>
-          )}
-          <Text>
-            좌표 Lat {item.item.noorLat} / Lon {item.item.noorLon}
-          </Text>
+        <View>
+            <View style={styles.searchArea}>
+                <TextInput
+                    onChangeText={onTextChange}
+                    style={styles.searchInput}
+                />
+                <Button title="Request" onPress={request} />
+            </View>
+            <FlatList
+                style={styles.flatList}
+                keyExtractor={item => item.pKey}
+                data={res}
+                renderItem={cellRender}
+                ListFooterComponent={loading && <ActivityIndicator />}
+            />
         </View>
-      </View>
     );
-  }
-
-  return (
-    <View>
-      <View style={styles.searchArea}>
-        <TextInput onChangeText={onTextChange} style={styles.searchInput} />
-        <Button title="Request" onPress={request} />
-      </View>
-      <FlatList
-        style={styles.flatList}
-        keyExtractor={item => item.pKey}
-        data={res}
-        renderItem={cellRender}
-        ListFooterComponent={loading && <ActivityIndicator />}
-      />
-    </View>
-  );
 };
 
 const styles = StyleSheet.create({
-  cell: {
-    height: 60,
-    marginLeft: 20,
-    marginRight: 20,
-    justifyContent: 'center',
-  },
-  cellDivider: {
-    borderBottomWidth: 1,
-    borderColor: '#000',
-  },
-  cellText: {
-    color: '#000',
-  },
-  searchInput: {
-    backgroundColor: '#aaaaaa',
-    height: 40,
-    width: '80%',
-    color: '#fff',
-    padding: 10,
-  },
-  searchArea: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginHorizontal: 20,
-  },
-  flatList: {
-    borderWidth: 1,
-    borderColor: '#333',
-    marginTop: 10,
-  },
+    cell: {
+        height: 60,
+        marginLeft: 20,
+        marginRight: 20,
+        justifyContent: 'center',
+    },
+    cellDivider: {
+        borderBottomWidth: 1,
+        borderColor: '#000',
+    },
+    cellText: {
+        color: '#000',
+    },
+    searchInput: {
+        backgroundColor: '#aaaaaa',
+        height: 40,
+        width: '80%',
+        color: '#fff',
+        padding: 10,
+    },
+    searchArea: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+    },
+    flatList: {
+        borderWidth: 1,
+        borderColor: '#333',
+        marginTop: 10,
+    },
 });
 
 export default PoiView;
